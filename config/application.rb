93ecd2a25ffee65b7eeb5ab02a08f58ae2b9c4cc
Arkadiusz Szczepkowicz <arek.szczepkowicz@gmail.com>
2016-11-14 20:22:55 +0100

    fix assets

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,19 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# you've limited to :test, :development, or :production. #*Rails.groups
+Bundler.require(:default, :assets, Rails.env)
 
 module Chatter
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
